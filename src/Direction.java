259d30a9c6c3c9ab7c187e346eb1ac9736887b1b
Mujahidul-Islam <40883770+Mujahidul-Islam@users.noreply.github.com>
2020-10-17 18:58:11 -0500

    ArrayList Creation

--- src/Direction.java
+++ src/Direction.java
@@ -8,16 +8,37 @@ public Direction ( int rowChange, int columnChange){
     this.columnChange = columnChange;
 }
 
-    public int getColumnChange() {
-        return columnChange;
-    }
 
     public int getRowChange() {
         return rowChange;
     }
+    public int getColumnChange() {
+        return columnChange;
+    }
+
+
 
     @Override
     public String toString() {
-        
+
+    String str ;
+
+    if(rowChange == -1 && columnChange == 1){
+        str ="<up right>";
+
+    }
+    else if (rowChange == 1 && columnChange == -1){
+        str = "<down left>";
+    }
+
+    else if(rowChange == 1 && columnChange == 0){
+        str = "<down>";
+
+    }
+    else  {
+
+        str = "<left>";
+        }
+     return str;
     }
 }
---
+++ src/DirectionList.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class DirectionList {
+    private ArrayList<Integer> array = new ArrayList<Integer>();
+}
